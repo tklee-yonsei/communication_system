a54a2ed8274792460d13a767f274cb42aed94a42
TenKeyLess <blivme84@naver.com>
2024-06-26 14:57:17 +0900

    * Sample implementation

--- noise/awgn_noise.py
+++ noise/awgn_noise.py
@@ -5,7 +5,7 @@ from noise.noise_interface import NoiseInterface
 
 class AWGNNoise(NoiseInterface):
     def __init__(self, snr_db):
-        raise NotImplementedError("AWGNNoise의 생성자를 구현하여야 합니다.")
+        self.snr_db = snr_db
 
     def apply(self, signal):
-        raise NotImplementedError("AWGNNoise의 apply 메소드를 구현하여야 합니다.")
+        return signal + 0.1
