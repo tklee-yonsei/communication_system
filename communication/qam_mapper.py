790d7f0bfc8d0c4d6b41766c09cf7ea6ec7bf32a
TenKeyLess <blivme84@naver.com>
2024-06-26 20:30:27 +0900

    * Implement qam mapper

--- communication/qam_mapper.py
+++ communication/qam_mapper.py
@@ -1,6 +1,28 @@
+import numpy as np
+
 from communication.mapper_interface import MapperInterface
 
 
 class QAMMapper(MapperInterface):
     def map(self, data):
-        raise NotImplementedError("QAMMapper map 메소드를 구현하여야 합니다.")
+        # Example of 16-QAM mapping
+        symbol_map = {
+            (0, 0, 0, 0): -3 - 3j,
+            (0, 0, 0, 1): -3 - 1j,
+            (0, 0, 1, 0): -3 + 3j,
+            (0, 0, 1, 1): -3 + 1j,
+            (0, 1, 0, 0): -1 - 3j,
+            (0, 1, 0, 1): -1 - 1j,
+            (0, 1, 1, 0): -1 + 3j,
+            (0, 1, 1, 1): -1 + 1j,
+            (1, 0, 0, 0): 3 - 3j,
+            (1, 0, 0, 1): 3 - 1j,
+            (1, 0, 1, 0): 3 + 3j,
+            (1, 0, 1, 1): 3 + 1j,
+            (1, 1, 0, 0): 1 - 3j,
+            (1, 1, 0, 1): 1 - 1j,
+            (1, 1, 1, 0): 1 + 3j,
+            (1, 1, 1, 1): 1 + 1j,
+        }
+        mapped_data = [symbol_map[tuple(bits)] for bits in data.reshape(-1, 4)]
+        return np.array(mapped_data)
--- tests/communication/test_qam_mapper.py
+++ tests/communication/test_qam_mapper.py
@@ -8,4 +8,6 @@ def test_qam_mapper():
     mapper = QAMMapper()
     data = np.array([0, 0, 0, 0, 1, 1, 1, 1])
     mapped_data = np.array([-3 - 3j, 1 + 1j])
+    # data = np.array([0, 1, 1, 0])
+    # mapped_data = np.array([-1, 1, 1, -1])
     map_and_test(mapper, data, mapped_data)
