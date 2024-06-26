3775eddc4c4f2d353c2f097cb813f6d36652add5
TenKeyLess <blivme84@naver.com>
2024-06-26 14:08:23 +0900

    * Code clean for noise interface

--- tests/noise/test_noise_interface.py
+++ tests/noise/test_noise_interface.py
@@ -1,9 +1,15 @@
 import numpy as np
+
 from noise.noise_interface import NoiseInterface
 
-def apply_noise_and_test(noise_instance, signal, should_equal):
+
+def apply_noise_and_test(noise_instance: NoiseInterface, signal, should_equal):
     noisy_signal = noise_instance.apply(signal)
     if should_equal:
-        assert np.array_equal(signal, noisy_signal), f"{noise_instance.__class__.__name__} failed"
+        assert np.array_equal(
+            signal, noisy_signal
+        ), f"{noise_instance.__class__.__name__} failed"
     else:
-        assert not np.array_equal(signal, noisy_signal), f"{noise_instance.__class__.__name__} failed"
+        assert not np.array_equal(
+            signal, noisy_signal
+        ), f"{noise_instance.__class__.__name__} failed"
